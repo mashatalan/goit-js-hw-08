2f766a52de4c6e1ccf67d8d2a28668aed4632999
Maria Talan <mashenka.talan@gmail.com>
2023-04-28 18:45:29 +0300

    Completed task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+'use strict';
 
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
+
+
+const galleryList = document.querySelector('.gallery');
+const galleryMarkup = galleryItems.map(item => {
+  return `<li class='gallery__item'>
+      <a
+        class='gallery__link'
+        href='${item.original}'
+      >
+        <img
+          class='gallery__image'
+          src='${item.preview}'
+          alt='${item.description}'
+        />
+      </a>
+    </li>`;
+}).join('');
+
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
